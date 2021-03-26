1f9f5df812ba3b27e6181a8676d12a6c74e449ae
AzKo <sdk1nv2@gmail.com>
2021-03-26 13:18:03 +0600

    init: start basic js task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+  
+  let cats = '^^';
+  let counter = 0;
+  for (let countArrayCurrentD = 0; countArrayCurrentD < matrix.length; countArrayCurrentD++ ){
+       let arrayElemOne = matrix[countArrayCurrentD]; // это теперь у нас отдельный взятый первый элемент массива !!!!!!!!!!!
+       for (let countElemsOFarrayOne = 0; countElemsOFarrayOne < arrayElemOne.length; countElemsOFarrayOne++){
+         if (arrayElemOne[countElemsOFarrayOne] == cats){
+           counter++;
+         }
+       }
+  }
+  return counter;
   // remove line with error and write your code here
 };
